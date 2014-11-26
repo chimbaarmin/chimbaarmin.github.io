e7b689ec964837f0fa30325deaea6396af57a66a
chimbaarmin <chimba.armin@gmail.com>
2014-11-26 09:33:19 +0100

    Coms v0.4

--- coms/coms.js
+++ coms/coms.js
@@ -1,22 +1,22 @@
 hshtg = /#([a-zA-Z0-9_]+)/g;
 
-            function linkHashtags(text) {
-                return text.replace(
-                    hshtg,
-                    '<span class="hshtg">#$1</span>'
-                );
-            }
+function linkHashtags(text) {
+    return text.replace(
+    hshtg,
+    '<span class="hshtg">#$1</span>'
+    );
+}
 
-            $(document).ready(function(){
-                $('p').each(function() {
-                    $(this).html(linkHashtags($(this).html()));
-                });
-            });
-            $.fn.lastWord = function() {
-              var text = this.text().trim().split(" ");
-              var last = text.pop();
-              this.html(text.join(" ") + (text.length > 0 ? " <span class='comnum'>" + last + "</span>" : last));
-            };
-            $(document).ready(function(){
-            $("h4").lastWord();
-            });
+$(document).ready(function(){
+    $('p').each(function() {
+        $(this).html(linkHashtags($(this).html()));
+    });
+});
+$.fn.lastWord = function() {
+      var text = this.text().trim().split(" ");
+      var last = text.pop();
+      this.html(text.join(" ") + (text.length > 0 ? " <span class='comnum'>" + last + "</span>" : last));
+};
+$(document).ready(function(){
+    $("h4").lastWord();
+});
