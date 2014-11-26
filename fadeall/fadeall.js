a32fb80eaf3caf5af83ca4db4afe36f166bc1a6c
chimbaarmin <chimba.armin@gmail.com>
2014-11-26 10:47:07 +0100

    Fadeall v0.4

--- fadeall/fadeall.js
+++ fadeall/fadeall.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
-                    $("#the_lights").fadeTo(1,0);
-                    $(".brightbox").mouseenter(function () {
-                         document.getElementById("the_lights").style.display="block";
-                         $("#the_lights").fadeTo("slow",0.8);
-                    });
-                    $(".brightbox").mouseleave(function () {
-                         document.getElementById("the_lights").style.display="block";
-                         $("#the_lights").fadeTo("slow",0);
-                    });
-               });
+    $("#the_lights").fadeTo(1,0);
+    $(".brightbox").mouseenter(function () {
+        $('.brightbox').not(this).removeClass('standout');
+        document.getElementById("the_lights").style.display="block";
+        $("#the_lights").fadeTo("slow",0.8);
+    });
+    $(".brightbox").mouseleave(function () {
+        document.getElementById("the_lights").style.display="block";
+        $("#the_lights").fadeTo("slow",0, function () {
+            $('.brightbox').not(this).addClass('standout');
+        });
+                        
+    });
+});
