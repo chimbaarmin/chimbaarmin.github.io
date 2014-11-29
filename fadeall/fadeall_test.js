ea10f41609a6a3ee2a6d2f6352ec83919d58dd2e
chimbaarmin <chimba.armin@gmail.com>
2014-11-29 11:53:43 +0100

    FD

--- fadeall/fadeall_test.js
+++ fadeall/fadeall_test.js
@@ -1,27 +1,32 @@
 $(document).ready(function(){
     $("#the_lights").fadeTo(1,0);
-    $(".brightbox").mouseenter(function () {
-        $('.brightbox').not(this).css({"z-index":"0"});;
+    $(".theSelector").mouseenter(function () {
+        $('.theSelector').not(this).css({"z-index":"0"});;
         document.getElementById("the_lights").style.display="block";
         $("#the_lights").fadeTo("slow",0.8);
     });
-    $(".brightbox").mouseleave(function () {
+    $(".theSelector").mouseleave(function () {
         document.getElementById("the_lights").style.display="block";
         $("#the_lights").fadeTo("slow",0, function () {
-            $('.brightbox').not(this).css({"z-index":"1000"});
+            $('.theSelector').not(this).css({"z-index":"1000"});
         });
                         
     });
 });
 
 $(document).ready(function(){
-    $( ".brightbox" ).css({"background-color":"#fff"});
-    $( ".brightbox" ).css({"position":"relative"});
-    $( ".brightbox" ).css({"z-index":"1000"});
+    $( "#the_lights" ).css({"background-color":"#000"});
+    $( "#the_lights" ).css({"height":"100%"});
+    $( "#the_lights" ).css({"width":"100%"});
+    $( "#the_lights" ).css({"position":"fixed"});
+    $( "#the_lights" ).css({"top":"0"});
+    $( "#the_lights" ).css({"left":"0"});
+    $( "#the_lights" ).css({"display":"none"});
 });
-
-var the_lights = '';
 
 $(document).ready(function(){
-    $('body').append(the_lights);
+    $( ".theSelector" ).css({"background-color":"#fff"});
+    $( ".theSelector" ).css({"position":"relative"});
+    $( ".theSelector" ).css({"z-index":"1000"});
 });
+
