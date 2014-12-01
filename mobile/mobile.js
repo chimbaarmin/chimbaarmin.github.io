b3f0ab620efcd0bb8ffedf1e8e768be29010b315
chimbaarmin <chimba.armin@gmail.com>
2014-12-01 19:31:32 +0100

    fadein delay

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
     setTimeout(function() {
         $(".liaa").fadeOut("slow", function() {
             $(".liaa").remove();
-            $(".ni-search").fadeIn("slow");
-            $(".lia").fadeIn("slow");
-            $(".lib").fadeIn("slow");
-            $(".lic").fadeIn("slow");
+            $(".ni-search").fadeIn( 1000, function() {
+            $(".lia").fadeIn( 1000, function() {
+            $(".lib").fadeIn( 1000, function() {
+            $(".lic").fadeIn( 1000 );
+                    });
+                });
+            });
         });
-    }, 800); 
-                        
+    }, 800);                      
 });
