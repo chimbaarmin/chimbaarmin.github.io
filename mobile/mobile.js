91f52cbfc94864e8509b836b9383ae386a662429
chimbaarmin <chimba.armin@gmail.com>
2014-12-02 17:47:15 +0100

    comments

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -13,22 +13,7 @@ $(document).ready(function() {
     }, 800);                      
 });
 
-hshtg = /#([a-zA-Z0-9_]+)/g;
-
-function linkHashtags(text) {
-    return text.replace(
-    hshtg,
-    '<span class="hshtg">#$1</span>'
-    );
-}
-
-$(document).ready(function(){
-    $('.comment-box > p').each(function() {
-        $(this).html(linkHashtags($(this).html()));
-    });
+$(document).ready(function() {
+    function fadeLI(elem) { elem.fadeIn(800, function() { fadeLI($(this).next()); }); }
+    fadeLI($("#comments li:first"));
 });
-$.fn.lastWord = function() {
-      var text = this.text().trim().split(" ");
-      var last = text.pop();
-      this.html(text.join(" ") + (text.length > 0 ? " <span class='comnum'>" + last + "</span>" : last));
-};
