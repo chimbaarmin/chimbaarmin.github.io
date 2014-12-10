a0089587ecaa964c16c636f3f6e52a222dc8d913
chimbaarmin <chimba.armin@gmail.com>
2014-12-10 12:40:00 +0100

    gate2

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     $("button.step1_vote_item.step1_vote_up").append(html1);
     $("button.step1_vote_item.step1_vote_down").append(html2);
     $("div, h1, h2, h3, h4, h5, h6, p").removeAttr("style");
+    $("div.step1_subscr_details_wrapper > input").removeAttr("style");
     $("step1_vote_box").wrap('<div class="centereddiv"></div>');
     $("button.step1_call_type_item").addClass('btn btn-default btn-sm');
     $("div.step1_call_type_wrapper").addClass('btn-group');
