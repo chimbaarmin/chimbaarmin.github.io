da4d75bff1ff24ff295e5134bb309dc3ad2fdfaf
chimbaarmin <chimba.armin@gmail.com>
2014-12-11 10:43:28 +0100

    unwrapper test

---
+++ gateway_test/gateway/gateway.js
@@ -0,0 +1,34 @@
+var html1 = '<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>',
+    html2 = '<span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span>';
+
+$(document).ready(function(){
+    $("button.step1_vote_item.step1_vote_up").append(html1);
+    $("button.step1_vote_item.step1_vote_down").append(html2);
+    $("div, h1, h2, h3, h4, h5, h6, p").removeAttr("style");
+    $("div.step1_subscr_details_wrapper > input").removeAttr("style");
+    $("step1_vote_box").wrap('<div class="centereddiv"></div>');
+    $("button.step1_call_type_item").addClass('btn btn-default');
+    $("div.step1_call_type_wrapper").addClass('btn-group-xs');
+    $("div.step1_radio_group").addClass('input-group');
+    $("span.step1_dd_tumbler").wrap('<div class="spandiv"></div>');
+    $("textarea#step1_comment_text").addClass('form-control');
+    $("input.gateway_submit_button").addClass('btn');
+    $("div.step1_group_wrapper").css("display", "none");
+    $( ".spandiv" ).click(function() {
+       $( this ).next().next().toggle();
+    });
+    $("form.step1 > h1").wrap('<div class="ribbon_frame"></div>');
+    $("h6").wrap('<div class="ribbon_frame_small"></div>');
+    $( "div.ribbon_frame" ).after( '<div class="triangle-1"></div>' );
+    $("form.step2 > h1").wrap('<div class="ribbon_frame_middle"></div>');
+    $("#gateway > div.subtstep_2.substep > div > div > input[type='text']").addClass("form-control");
+    $("div.step1_category_box_item").wrapAll('<div class="frame_menu"></div>');
+    $("div.frame_menu").before('<span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span><div class="frame_menu_title">Kategorien ▾</div>');
+    $( "div.frame_menu_title" ).hover(function() {
+        $("div.frame_menu").show();
+    });
+    $(".step3 > h1").wrap('<div class="ribbon_frame_middle"></div>');
+    $("#gateway > div.gateway_sbm_btn_wrapper > button").addClass('btn');
+    $("#gateway > div.step2_content_wrapper > div.step2_cpc_item").addClass('col-md-6');
+});
+
---
+++ gateway_test/gateway/unwrapper.js
@@ -0,0 +1,9 @@
+/* Nummer-Index.De Unwrapper v1.0 */
+
+$(document).ready(function(){
+    $("body > div > div > div.col-md-6 > div.outer > div").unwrap('');
+    $("body > div > div > div.col-md-6 > div.middle > div").unwrap('');
+    $("#layout").unwrap('');
+});
+
+
