c31c1e96fcb389a456de9f940b20272b2affe5c4
chimbaarmin <chimba.armin@gmail.com>
2015-03-05 14:41:49 +0100

    test

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -264,55 +264,45 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 	jQuery( "li.content_city_list_li.city_one_link" ).click(function() {
+		jQuery( '.content_number_list_one, .content_number_list_two, .content_number_list_three, .content_number_list_four, .content_number_list_five' ).css( 'display', 'none' );
 		jQuery( '.content_number_list_one' ).css( 'display', 'block' );
-		jQuery( '.content_number_list_two' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_three' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_four' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_five' ).css( 'display', 'none' );
-		
+		jQuery( 'li.content_city_list_li.city_one_link, li.content_city_list_li.city_two_link, li.content_city_list_li.city_three_link, li.content_city_list_li.city_four_link, li.content_city_list_li.city_five_link' ).removeClass( 'active_city' );
+		jQuery( 'li.content_city_list_li.city_one_link' ).addClass( 'active_city' );
 	});
 });
 
 $(document).ready(function() {
 	jQuery( "li.content_city_list_li.city_two_link" ).click(function() {
-		jQuery( '.content_number_list_one' ).css( 'display', 'none' );
+		jQuery( '.content_number_list_one, .content_number_list_two, .content_number_list_three, .content_number_list_four, .content_number_list_five' ).css( 'display', 'none' );
 		jQuery( '.content_number_list_two' ).css( 'display', 'block' );
-		jQuery( '.content_number_list_three' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_four' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_five' ).css( 'display', 'none' );
-		
+		jQuery( 'li.content_city_list_li.city_one_link, li.content_city_list_li.city_two_link, li.content_city_list_li.city_three_link, li.content_city_list_li.city_four_link, li.content_city_list_li.city_five_link' ).removeClass( 'active_city' );
+		jQuery( 'li.content_city_list_li.city_two_link' ).addClass( 'active_city' );
 	});
 });
 
 $(document).ready(function() {
 	jQuery( "li.content_city_list_li.city_three_link" ).click(function() {
-		jQuery( '.content_number_list_one' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_two' ).css( 'display', 'none' );
+		jQuery( '.content_number_list_one, .content_number_list_two, .content_number_list_three, .content_number_list_four, .content_number_list_five' ).css( 'display', 'none' );
 		jQuery( '.content_number_list_three' ).css( 'display', 'block' );
-		jQuery( '.content_number_list_four' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_five' ).css( 'display', 'none' );
-		
+		jQuery( 'li.content_city_list_li.city_one_link, li.content_city_list_li.city_two_link, li.content_city_list_li.city_three_link, li.content_city_list_li.city_four_link, li.content_city_list_li.city_five_link' ).removeClass( 'active_city' );
+		jQuery( 'li.content_city_list_li.city_three_link' ).addClass( 'active_city' );
 	});
 });
 
 $(document).ready(function() {
 	jQuery( "li.content_city_list_li.city_four_link" ).click(function() {
-		jQuery( '.content_number_list_one' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_two' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_three' ).css( 'display', 'none' );
+		jQuery( '.content_number_list_one, .content_number_list_two, .content_number_list_three, .content_number_list_four, .content_number_list_five' ).css( 'display', 'none' );
 		jQuery( '.content_number_list_four' ).css( 'display', 'block' );
-		jQuery( '.content_number_list_five' ).css( 'display', 'none' );
-		
+		jQuery( 'li.content_city_list_li.city_one_link, li.content_city_list_li.city_two_link, li.content_city_list_li.city_three_link, li.content_city_list_li.city_four_link, li.content_city_list_li.city_five_link' ).removeClass( 'active_city' );
+		jQuery( 'li.content_city_list_li.city_four_link' ).addClass( 'active_city' );
 	});
 });
 
 $(document).ready(function() {
 	jQuery( "li.content_city_list_li.city_five_link" ).click(function() {
-		jQuery( '.content_number_list_one' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_two' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_three' ).css( 'display', 'none' );
-		jQuery( '.content_number_list_four' ).css( 'display', 'none' );
+		jQuery( '.content_number_list_one, .content_number_list_two, .content_number_list_three, .content_number_list_four, .content_number_list_five' ).css( 'display', 'none' );
 		jQuery( '.content_number_list_five' ).css( 'display', 'block' );
-		
+		jQuery( 'li.content_city_list_li.city_one_link, li.content_city_list_li.city_two_link, li.content_city_list_li.city_three_link, li.content_city_list_li.city_four_link, li.content_city_list_li.city_five_link' ).removeClass( 'active_city' );
+		jQuery( 'li.content_city_list_li.city_five_link' ).addClass( 'active_city' );
 	});
 });
