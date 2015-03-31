8d5e79ed9874cdd515351f4c6871d8b82947d76a
chimbaarmin <chimba.armin@gmail.com>
2015-03-31 14:02:20 +0200

    dnp update

--- dnp/desktop_number/desktop_number.js
+++ dnp/desktop_number/desktop_number.js
@@ -1,7 +1,7 @@
 /*
 Desktop number profile JS
 Author: Armin
-Date: 13. 03. 2015.
+Date: 31. 03. 2015.
 Version: 1.0
 Custom prefix: none
 Custom functions: none
@@ -66,47 +66,12 @@ jQuery(function($) {
 
 /* =Footer JS*/
 
-$(document).ready(function() {
-	jQuery( "li.dnp_data_number_cities_li.dnp_city_one" ).click(function() {
-		jQuery( 'li.dnp_data_number_cities_li.dnp_city_one, li.dnp_data_number_cities_li.dnp_city_two, li.dnp_data_number_cities_li.dnp_city_three, li.dnp_data_number_cities_li.dnp_city_four, li.dnp_data_number_cities_li.dnp_city_five' ).removeClass( 'dnp_active_city' );
-		jQuery( 'li.dnp_data_number_cities_li.dnp_city_one' ).addClass( 'dnp_active_city' );
-		jQuery( '.dnp_data_number_numbers_city_one, .dnp_data_number_numbers_city_two, .dnp_data_number_numbers_city_three, .dnp_data_number_numbers_city_four, .dnp_data_number_numbers_city_five' ).css( 'display', 'none' );
-		jQuery( '.dnp_data_number_numbers_city_one' ).css( 'display', 'block' );
-	});
-});
-
-$(document).ready(function() {
-	jQuery( "li.dnp_data_number_cities_li.dnp_city_two" ).click(function() {
-		jQuery( 'li.dnp_data_number_cities_li.dnp_city_one, li.dnp_data_number_cities_li.dnp_city_two, li.dnp_data_number_cities_li.dnp_city_three, li.dnp_data_number_cities_li.dnp_city_four, li.dnp_data_number_cities_li.dnp_city_five' ).removeClass( 'dnp_active_city' );
-		jQuery( 'li.dnp_data_number_cities_li.dnp_city_two' ).addClass( 'dnp_active_city' );
-		jQuery( '.dnp_data_number_numbers_city_one, .dnp_data_number_numbers_city_two, .dnp_data_number_numbers_city_three, .dnp_data_number_numbers_city_four, .dnp_data_number_numbers_city_five' ).css( 'display', 'none' );
-		jQuery( '.dnp_data_number_numbers_city_two' ).css( 'display', 'block' );
-	});
-});
-
-$(document).ready(function() {
-	jQuery( "li.dnp_data_number_cities_li.dnp_city_three" ).click(function() {
-		jQuery( 'li.dnp_data_number_cities_li.dnp_city_one, li.dnp_data_number_cities_li.dnp_city_two, li.dnp_data_number_cities_li.dnp_city_three, li.dnp_data_number_cities_li.dnp_city_four, li.dnp_data_number_cities_li.dnp_city_five' ).removeClass( 'dnp_active_city' );
-		jQuery( 'li.dnp_data_number_cities_li.dnp_city_three' ).addClass( 'dnp_active_city' );
-		jQuery( '.dnp_data_number_numbers_city_one, .dnp_data_number_numbers_city_two, .dnp_data_number_numbers_city_three, .dnp_data_number_numbers_city_four, .dnp_data_number_numbers_city_five' ).css( 'display', 'none' );
-		jQuery( '.dnp_data_number_numbers_city_three' ).css( 'display', 'block' );
-	});
-});
-
-$(document).ready(function() {
-	jQuery( "li.dnp_data_number_cities_li.dnp_city_four" ).click(function() {
-		jQuery( 'li.dnp_data_number_cities_li.dnp_city_one, li.dnp_data_number_cities_li.dnp_city_two, li.dnp_data_number_cities_li.dnp_city_three, li.dnp_data_number_cities_li.dnp_city_four, li.dnp_data_number_cities_li.dnp_city_five' ).removeClass( 'dnp_active_city' );
-		jQuery( 'li.dnp_data_number_cities_li.dnp_city_four' ).addClass( 'dnp_active_city' );
-		jQuery( '.dnp_data_number_numbers_city_one, .dnp_data_number_numbers_city_two, .dnp_data_number_numbers_city_three, .dnp_data_number_numbers_city_four, .dnp_data_number_numbers_city_five' ).css( 'display', 'none' );
-		jQuery( '.dnp_data_number_numbers_city_four' ).css( 'display', 'block' );
-	});
-});
-
-$(document).ready(function() {
-	jQuery( "li.dnp_data_number_cities_li.dnp_city_five" ).click(function() {
-		jQuery( 'li.dnp_data_number_cities_li.dnp_city_one, li.dnp_data_number_cities_li.dnp_city_two, li.dnp_data_number_cities_li.dnp_city_three, li.dnp_data_number_cities_li.dnp_city_four, li.dnp_data_number_cities_li.dnp_city_five' ).removeClass( 'dnp_active_city' );
-		jQuery( 'li.dnp_data_number_cities_li.dnp_city_five' ).addClass( 'dnp_active_city' );
-		jQuery( '.dnp_data_number_numbers_city_one, .dnp_data_number_numbers_city_two, .dnp_data_number_numbers_city_three, .dnp_data_number_numbers_city_four, .dnp_data_number_numbers_city_five' ).css( 'display', 'none' );
-		jQuery( '.dnp_data_number_numbers_city_five' ).css( 'display', 'block' );
+jQuery(document).ready(function(){
+	jQuery( '.dnp_new_footer_trigger' ).click(function() {
+		var $activeItem = jQuery( this ).attr( 'data-item' );
+		jQuery( '.dnp_new_footer_trigger' ).removeClass('active_button');
+		jQuery( this ).addClass('active_button');
+		jQuery ( '.dnp_new_footer_numbers_wrapper' ).removeClass('active_item');
+		jQuery( "." + $activeItem ).addClass('active_item');
 	});
 });
