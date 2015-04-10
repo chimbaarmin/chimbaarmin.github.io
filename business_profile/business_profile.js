c48b17a085b440e7f8a967b3cc4ea340e0417003
chimbaarmin <chimba.armin@gmail.com>
2015-04-10 16:45:28 +0200

    aaaa

---
+++ business_profile/business_profile.js
@@ -0,0 +1,21 @@
+/* =Footer JS*/
+
+jQuery(document).ready(function(){
+	jQuery( '.business_footer_module_trigger' ).click(function() {
+		var $activeItem = jQuery( this ).attr( 'data-item' );
+		jQuery( '.business_footer_module_trigger' ).removeClass('footer_trigger_active');
+		jQuery( this ).addClass('footer_trigger_active');
+		jQuery ( '.business_footer_module_sheet' ).removeClass('footer_sheet_active');
+		jQuery( "." + $activeItem ).addClass('footer_sheet_active');
+	});
+});
+
+jQuery(document).ready(function(){
+	jQuery( '.fotter_sheet_buttons_link' ).click(function() {
+		var $activeItem = jQuery( this ).attr( 'data-item' );
+		jQuery( '.fotter_sheet_buttons_link' ).removeClass('buttons_link_active');
+		jQuery( this ).addClass('buttons_link_active');
+		jQuery ( '.footer_sheet_numbers' ).removeClass('sheet_numbers_active');
+		jQuery( "." + $activeItem ).addClass('sheet_numbers_active');
+	});
+});
