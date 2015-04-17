e8f9375f19d92803b2f971e449e79231a2c56458
chimbaarmin <chimba.armin@gmail.com>
2015-04-17 17:04:15 +0200

    test zoom 5 > 10

--- mobile_app/mobile_homepage.js
+++ mobile_app/mobile_homepage.js
@@ -89,7 +89,7 @@ function initialize(condition) {
 	var userLatlng = new google.maps.LatLng( $userLa, $userLo); 
 	
 	var myOptions = {
-    	zoom: 5,
+    	zoom: 10,
 		center: userLatlng,
 		disableDefaultUI: true,
     	mapTypeId: google.maps.MapTypeId.ROADMAP
