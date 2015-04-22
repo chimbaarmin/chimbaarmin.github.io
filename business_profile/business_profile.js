0a052ab59c6d1d168fcb66cf7eb65b729f6236a1
chimbaarmin <chimba.armin@gmail.com>
2015-04-22 13:21:01 +0200

    test

--- business_profile/business_profile.js
+++ business_profile/business_profile.js
@@ -1,3 +1,78 @@
+/*
+Business page JS
+Date: 22. 04. 2015.
+Author: Armin
+Version: 1.0
+*/
+
+/* =Google Maps */
+
+jQuery( document ).ready(function() {
+	var myLatLng = new google.maps.LatLng(52.5128318, 13.4656515);
+	var map;
+	var myOptions = {
+		zoom: 15,
+		center: myLatLng,
+		disableDefaultUI: true
+	};
+	var styles = [
+	  	{
+			stylers: [
+				 { saturation: 0 },
+		 	]
+	  	}
+	];
+			
+	map = new google.maps.Map($('.google_map')[0], myOptions);
+	map.setOptions({styles: styles});
+	
+	var marker = new google.maps.Marker({
+		  position: myLatLng,
+		  map: map,
+		  title: 'Poshte Boom'
+	});
+	
+	google.maps.event.addDomListener(window, 'load');
+});
+	
+/* =Gallery */
+
+Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
+
+Galleria.configure({
+	transition: 'fade',
+	imageCrop: true,
+	autoplay: 3000,
+	thumbnails: true,
+	height: 300,
+	lightbox: true
+});
+
+Galleria.run('.galleria');
+
+/* Right menu */
+
+jQuery(function($) {
+    function fixDiv() {
+		var $cache = $('#getFixed');
+		var elemheight = document.getElementById('getFixed').offsetHeight;
+		var diffheight = elemheight - window.innerHeight + 40;
+		if ($(window).scrollTop() >  diffheight) {
+	  		$cache.css({
+				'top': '-' + diffheight +'px',
+				'position': 'fixed'
+	  		});
+		} else {
+	  		$cache.css({
+			'position': 'relative',
+			'top': 'auto'
+	  		}); 
+		}
+  	}
+    $(window).scroll(fixDiv);
+  	fixDiv();
+});
+
 /* =Footer JS*/
 
 jQuery(document).ready(function(){
